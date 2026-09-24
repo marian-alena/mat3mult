319d5da52de443ae357ecb96bec2c848a5af4014
marian-alena <m.alenamath@gmail.com>
2026-09-24 11:09:34 -0700

    "Applied optimization"

--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@
 #include "time.h"
 
 int main(){
-	clock_t start,end;
-	size_t n=1000;
-	Eigen::MatrixXd a=Eigen::MatrixXd::Random(n,n);
-	Eigen::MatrixXd b=Eigen::MatrixXd::Random(n,n);
+	clock_t start, end;
+	size_t n = 1000;
+    size_t mLoops = 10;
+	Eigen::MatrixXd a = Eigen::MatrixXd::Random(n,n);
+	Eigen::MatrixXd b = Eigen::MatrixXd::Random(n,n);
+    Eigen::MatrixXd c;
 
-	// perform matrix multiplication and time it
-	std::cout<<"Starting matrix multiplication..."<<std::endl;
-	start=clock();
-	Eigen::MatrixXd c=a*b;
-	end=clock();
-	std::cout<<(double)(end-start)/CLOCKS_PER_SEC<<std::endl;
+	// perform matrix multiplication for loops and time it
+	std::cout << "Starting matrix multiplication, " << mLoops << " loops..." << std::endl;
+	start = clock();
+    for (size_t i = 0; i < mLoops; i++)
+    {
+	    c = a * b;
+    }
+	end = clock();
+	std::cout << (double)(end - start)/(CLOCKS_PER_SEC * mLoops) << std::endl;
 
 	return 0;
 }
