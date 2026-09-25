63693e91fa44387eb8a67524244324e40f8d19de
marian-alena <m.alenamath@gmail.com>
2026-09-25 00:13:34 -0700

    "Added version of compiler"

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 #include "time.h"
 
 int main(){
+    // Check for Clang
+    #if defined(__clang__)
+        std::cout << "Compiler: Clang\n";
+        std::cout << "Version: " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__ << "\n";
+    // Check for GCC
+    #elif defined(__GNUC__)
+        std::cout << "Compiler: GCC\n";
+        std::cout << "Version: " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "\n";
+    // Check for Microsoft Visual C++
+    #elif defined(_MSC_VER)
+        std::cout << "Compiler: MSVC\n";
+        std::cout << "Internal Version Toolset: " << _MSC_VER << "\n";
+    #else
+        std::cout << "Unknown Compiler\n";
+    #endif
+
 	clock_t start, end;
 	size_t n = 1000;
     size_t mLoops = 10;
@@ -18,7 +34,7 @@ int main(){
 	    c = a * b;
     }
 	end = clock();
-	std::cout << (double)(end - start)/(CLOCKS_PER_SEC * mLoops) << std::endl;
+	std::cout << (double)(end - start)/(CLOCKS_PER_SEC * mLoops) << " seconds per loop" << std::endl;
 
 	return 0;
 }
